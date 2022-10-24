68aec66d579117f8df5abe417a71371370510e2b
James Mburu Githire <jamesmburug24@gmail.com>
2022-10-24 06:21:47 +0300

    feature : Association Methods

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,4 +1,11 @@
 class Company < ActiveRecord::Base
     has_many :freebies
     has_many :devs, through: :freebies
+    def give_freebie(dev, item_name, value)
+        Freebie.create(item_name: item_name, value: value, dev: Dev.find_by(name: dev.name))
+    end
+
+    def oldest_company
+        Company.minimum("founding_year")
+    end
 end
