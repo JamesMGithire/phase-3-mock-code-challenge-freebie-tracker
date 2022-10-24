1f8830a317776c31a89b33a740d81a22c1605e0c
James Mburu Githire <jamesmburug24@gmail.com>
2022-10-24 06:15:41 +0300

    fix : association methods

--- app/models/freebie.rb
+++ app/models/freebie.rb
@@ -2,6 +2,14 @@ class Freebie < ActiveRecord::Base
     belongs_to :devs
     belongs_to :companies
     def dev
-        Dev.
+        Dev.find(self.dev_id)
+    end
+
+    def company
+        Company.find(self.company_id)
+    end
+    
+    def print_details
+        puts "#{self.dev.name} owns a #{self.item_name} from #{self.company.name}"
     end
 end
