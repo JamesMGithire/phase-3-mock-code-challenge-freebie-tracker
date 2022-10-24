c321f4ff946abd1ca2c8be7665c5c7917d3fbb2c
James Mburu Githire <jamesmburug24@gmail.com>
2022-10-24 06:32:05 +0300

    major : completed code-challenge

--- app/models/dev.rb
+++ app/models/dev.rb
@@ -1,4 +1,13 @@
 class Dev < ActiveRecord::Base
     has_many :freebies
     has_many :companies, through: :freebies
+
+    def received_one? item_name
+        id = Dev.find_by(name: self.name).id
+        Freebies.all.any? { |f| f.item_name == item_name && dev_id == id}
+    end
+    def give_away(dev, freebie)
+        id = Dev.find_by(name: self.name).id
+        freebie.dev_id == id && (dev.freebies << freebie)
+    end
 end
